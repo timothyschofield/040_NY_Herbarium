20cf20cb05da0c20f11e022db832b37e778ee463
Tim Schofield <timothyschofield@hotmail.com>
2024-06-18 11:42:56 +0100

    Tuesday morning

--- main_ny_herbarium.py
+++ main_ny_herbarium.py
@@ -101,13 +101,9 @@ import json
 import sys
 print(f"Python version {sys.version}")
 
-# from url_to_transcribe_ny_sweetgum_1000 import URL_PATH_LIST
-# from url_transcribed_ny_sweetgum import URL_PATH_LIST
-
 MODEL = "gpt-4o" # Context window of 128k max_tokens 4096
 
-load_dotenv() 
-
+load_dotenv()
 try:
     my_api_key = os.environ['OPENAI_API_KEY']          
     client = OpenAI(api_key=my_api_key)
@@ -220,30 +216,6 @@ else:
     print("ERROR: df_column_names is NOT a subset of df_to_transcribe_keys - BAD")
     exit()
 
-# Before imporovment
-"""
-prompt = (
-    f"Read this herbarium sheet and extract all the text you can"
-    f"The herbarium sheet may sometimes use Spanish, French or German"
-    f"Go through the text you have extracted and return data in JSON format with {keys_concatenated} as keys"
-    f"Use exactly {keys_concatenated} as keys"
-    f"Return the text you have extracted in the field 'OCR Text'"
-    f"'Collection Team' should contain other people involved in collecting the specimen"
-    f"The 'Collection Date To' and 'Collection Date From' should have the format YYYY-MM-DD"
-    f"If there is only one date then fill in 'Collection Date To' and 'Collection Date From' with the same value"
-    f"If no Continent is mentioned then infer it from the Country"
-    f"If no Country is mentioned then infer it from the Province, County or Locality Description"
-    f"If no Latitude or Longitude information is available then infer it as accurately as possible from the Locality Description, County, Province and Country"
-    f"If Latitude and Longitude have been inferred, fill in the 'Coordinate Uncertainty In Meters' with an estimate of the accuracy"
-    f"If a single elevation or altitude is mentioned fill in both the 'Minimum Elevation (Meters)' and 'Maximum Elevation (Meters)' with the same value"
-    f"If there is elevation information in Meters then do a conversion to feet and store the conversion in 'Minimum Elevation (Feet)' and 'Maximum Elevation (Feet)'"
-    f"For 'Plant Frequency' look for words like Abundant, Occasional, Common, Frequent or Rare" 
-    f"For 'Plant Substrate' look for what the plant grows on e.g. on rotting log, on damp rock, on bark"
-    f"If you can not find a value for a key return value 'none'"
-)
-"""
-
-
 
 # New - got it to estimate Lat and Lng from location
 prompt = (
@@ -262,7 +234,7 @@ prompt = (
     f"If there is only one date then fill in 'Collection Date To' and 'Collection Date From' with the same value"
     
     f"Infer the Continent field from the Country e.g. If the Country is 'Belize' then the Continent field is 'Central America', if the Country is 'Costa Rica' the the Continent field is 'South America'"
-    f"If no Country is mentioned then infer it from the Province, County or Locality Description"
+    f"If no Country is mentioned then infer it from the Continent, Province, County and Locality Description"
     
     f"If Latitude and Longitude are not mentioned in the text then infer them from the Country, Province, County and Locality Description"
     f"Put the infered Latitude and Longitude in the 'Latitude (Degrees Minutes Seconds)' 'Longitude (Degrees Minutes Seconds)' 'Latitude Decimal' and 'Longitude Decimal' fields"
@@ -271,12 +243,12 @@ prompt = (
     f"If a single elevation or altitude is mentioned fill in both the 'Minimum Elevation (Meters)' and 'Maximum Elevation (Meters)' with the same value"
     f"If there is elevation information in Meters then do a conversion to feet and store the conversion in 'Minimum Elevation (Feet)' and 'Maximum Elevation (Feet)'"
     
-    f"For 'Plant Frequency' field look for words like Abundant, Occasional, Common, Frequent or Rare"
-    f"Fot 'Plant Habitat' field put what type of environment the plant grows in e.g. forest, scrub, rocky hillside"
+    f"For 'Plant Frequency' field look for words like abundant, cccasional, common, frequent or rare"
+    f"For 'Plant Habitat' field put what type of environment the plant grows in e.g. forest, scrub, rocky hillside"
     f"For 'Plant Substrate' field look for what the plant grows on e.g. on rotting log, on damp rock, on bark"
     f"For 'Plant Description' field put a description of the plant e.g. Shrub 4m high, flowers white, fruit orange"
     
-    f"If a plant is cultivated put 'Yes' in the 'FeaCultivated? (Y/N)' field, otherwise put 'No'"
+    f"If a plant is cultivated put 'Yes' in the 'Cultivated' field, otherwise put 'No'"
     
     f"If you find 'INB' followed by a number add INB and the number to SpeOtherSpecimenNumbers_tab"
     
@@ -286,7 +258,7 @@ prompt = (
 
 headers = get_headers(my_api_key)
 
-source_type="url" # "url" or "local"
+source_type="local" # "url" or "local"
 print("####################################### START OUTPUT ######################################")
 for index, row in df_to_transcribe.iloc[0:].iterrows():  
 
