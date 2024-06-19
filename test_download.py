3787c324051519c8208efba50b387e2d19b597ad
Tim Schofield <timothyschofield@hotmail.com>
2024-06-19 17:03:46 +0100

    Search and replace char \u2032 ′ 8242 with ' to stop saving problem on Windows

--- test_download.py
+++ test_download.py
@@ -2,6 +2,42 @@ import requests
 from pathlib import Path
 import pandas as pd
 
+
+data = {"Name": ["T′m", "Nick", "K′ish", "Jack"],
+        "Month": ["January", "Ma′ch", "July", "August"],
+        "Place": ["Lan′aster", "Ca′d′n", "London", "P′ris"]
+        }
+
+df = pd.DataFrame(data)
+
+print(df)
+print("------")
+
+# Counts the number of instances of the string in the df
+sum_cols = 0
+for col in df:
+    sum_cols+= df[col].str.count("′").sum()
+print(sum_cols)
+
+# \u2032 ′ 8242
+print(ord("′")) # 8242
+
+print(chr(8242)) # ′
+print("------")
+
+# Replaces all instances of one string in a df with another
+df.replace({"′": "'"}, regex=True, inplace=True)
+
+
+print(df)
+print("------")
+
+
+
+exit()
+
+
+
 input_folder = "ny_herbarium_input"
 input_file = "NY_specimens_to_transcribe.csv"
 input_path = Path(f"{input_folder}/{input_file}")
